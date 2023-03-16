38f9538f9b8a4068a11b01a6331880d1ff679c65
Joe Qian <xiaochi.qian@balliol.ox.ac.uk>
2023-03-16 22:12:37 +0100

    fix

--- baird.py
+++ baird.py
@@ -67,7 +67,9 @@ def behavior_policy(state):
     return DASHED
 
 # Semi-gradient off-policy temporal difference
-# @state: current state
+# @state: state S_t
+# @action: action A_t
+# @next_state: state S_{t+1}
 # @theta: weight for each component of the feature vector
 # @alpha: step size
 # @return: next state
@@ -83,26 +85,10 @@ def semi_gradient_off_policy_TD(state, action, next_state, theta, alpha):
     # derivatives happen to be the same matrix due to the linearity
     theta += FEATURES[state, :] * delta
 
-# Semi-gradient DP
-# @theta: weight for each component of the feature vector
-# @alpha: step size
-def semi_gradient_DP(theta, alpha):
-    delta = 0.0
-    # go through all the states
-    for state in STATES:
-        expected_return = 0.0
-        # compute bellman error for each state
-        for next_state in STATES:
-            if next_state == LOWER_STATE:
-                expected_return += REWARD + DISCOUNT * np.dot(theta, FEATURES[next_state, :])
-        bellmanError = expected_return - np.dot(theta, FEATURES[state, :])
-        # accumulate gradients
-        delta += bellmanError * FEATURES[state, :]
-    # derivatives happen to be the same matrix due to the linearity
-    theta += alpha / len(STATES) * delta
-
 # temporal difference with gradient correction
-# @state: current state
+# @state: state S_t
+# @action: action A_t
+# @next_state: state S_{t+1}
 # @theta: weight of each component of the feature vector
 # @weight: auxiliary trace for gradient correction
 # @alpha: step size of @theta
@@ -118,59 +104,15 @@ def TDC(state, action, next_state, theta, weight, alpha, beta):
     theta += alpha * rho * (delta * FEATURES[state, :] - DISCOUNT * FEATURES[next_state, :] * np.dot(FEATURES[state, :], weight))
     weight += beta * rho * (delta - np.dot(FEATURES[state, :], weight)) * FEATURES[state, :]
 
-# expected temporal difference with gradient correction
+# direct gradient temporal difference
+# @state1: state S_t
+# @action1: action A_t 
+# @next_state1: state S_{t+1}
+# @state2: state S_{t+f(t)}
+# @action2: action A_{t+f(t)}
+# @next_state2: state 
 # @theta: weight of each component of the feature vector
-# @weight: auxiliary trace for gradient correction
 # @alpha: step size of @theta
-# @beta: step size of @weight
-def expected_TDC(theta, weight, alpha, beta):
-    for state in STATES:
-        # When computing expected update target, if next state is not lower state, importance ratio will be 0,
-        # so we can safely ignore this case and assume next state is always lower state
-        delta = REWARD + DISCOUNT * np.dot(FEATURES[LOWER_STATE, :], theta) - np.dot(FEATURES[state, :], theta)
-        rho = 1 / BEHAVIOR_SOLID_PROBABILITY
-        # Under behavior policy, state distribution is uniform, so the probability for each state is 1.0 / len(STATES)
-        expected_update_theta = 1.0 / len(STATES) * BEHAVIOR_SOLID_PROBABILITY * rho * (
-            delta * FEATURES[state, :] - DISCOUNT * FEATURES[LOWER_STATE, :] * np.dot(weight, FEATURES[state, :]))
-        theta += alpha * expected_update_theta
-        expected_update_weight = 1.0 / len(STATES) * BEHAVIOR_SOLID_PROBABILITY * rho * (
-            delta - np.dot(weight, FEATURES[state, :])) * FEATURES[state, :]
-        weight += beta * expected_update_weight
-
-    # if *accumulate* expected update and actually apply update here, then it's synchronous
-    # theta += alpha * expectedUpdateTheta
-    # weight += beta * expectedUpdateWeight
-
-# interest is 1 for every state
-INTEREST = 1
-
-# expected update of ETD
-# @theta: weight of each component of the feature vector
-# @emphasis: current emphasis
-# @alpha: step size of @theta
-# @return: expected next emphasis
-def expected_emphatic_TD(theta, emphasis, alpha):
-    # we perform synchronous update for both theta and emphasis
-    expected_update = 0
-    expected_next_emphasis = 0.0
-    # go through all the states
-    for state in STATES:
-        # compute rho(t-1)
-        if state == LOWER_STATE:
-            rho = 1.0 / BEHAVIOR_SOLID_PROBABILITY
-        else:
-            rho = 0
-        # update emphasis
-        next_emphasis = DISCOUNT * rho * emphasis + INTEREST
-        expected_next_emphasis += next_emphasis
-        # When computing expected update target, if next state is not lower state, importance ratio will be 0,
-        # so we can safely ignore this case and assume next state is always lower state
-        next_state = LOWER_STATE
-        delta = REWARD + DISCOUNT * np.dot(FEATURES[next_state, :], theta) - np.dot(FEATURES[state, :], theta)
-        expected_update += 1.0 / len(STATES) * BEHAVIOR_SOLID_PROBABILITY * next_emphasis * 1 / BEHAVIOR_SOLID_PROBABILITY * delta * FEATURES[state, :]
-    theta += alpha * expected_update
-    return expected_next_emphasis / len(STATES)
-
 def direct_GTD(state1, action1, next_state1, state2, action2, next_state2, theta, alpha):
     # get the importance ratios
     if action1 == DASHED:
@@ -209,15 +151,16 @@ def experiments():
     # Initialize the theta
     theta = np.ones(FEATURE_SIZE)
     theta[6] = 10
-
     theta_TD = copy.deepcopy(theta)
     theta_TDC = copy.deepcopy(theta)
     weight_TDC = np.zeros(FEATURE_SIZE)
     theta_DGTD = copy.deepcopy(theta)
 
+    # Stepsize configurations for each algorithm
     alpha_TD = 0.01
-    alpha_grad = 0.005
-    beta_grad = 0.05
+    alpha_TDC = 0.005
+    beta_TDC = 0.05
+    alpha_DGTD = 0.0005
 
     steps = 1000
     DGTD_step = 0
@@ -236,14 +179,15 @@ def experiments():
         history.append(next_state)
         # update the weights with each algorithms respectively
         semi_gradient_off_policy_TD(state, action, next_state, theta_TD, alpha_TD)
-        TDC(state, action, next_state, theta_TDC, weight_TDC, alpha_grad, beta_grad)
+        TDC(state, action, next_state, theta_TDC, weight_TDC, alpha_TDC, beta_TDC)
         while step >= DGTD_step + f_gap(DGTD_step):
             direct_GTD(history[2*DGTD_step], history[2*DGTD_step+1], history[2*DGTD_step+2],
                        history[2*(DGTD_step+f_gap(DGTD_step))], history[2*(DGTD_step+f_gap(DGTD_step))+1],
-                       history[2*(DGTD_step+f_gap(DGTD_step))+2], theta_DGTD, alpha_grad)
+                       history[2*(DGTD_step+f_gap(DGTD_step))+2], theta_DGTD, alpha_DGTD)
             DGTD_step+=1
+        # update the current state
         state = next_state
-        # Log corresponding errors for each algorithm
+        # log corresponding errors for each algorithm
         log = {
             'TD_RMSVE': compute_RMSVE(theta_TD), 
             'TD_RMSPBE': compute_RMSPBE(theta_TD), 
@@ -253,6 +197,7 @@ def experiments():
             'DGTD_RMSPBE': compute_RMSPBE(theta_DGTD),
         }
         logs = logs.append(log, ignore_index=True)
+    # save the logs
     logs.to_csv(f'./logs_approximated_seed[{SEED}].csv')
 
 if __name__ == '__main__':
